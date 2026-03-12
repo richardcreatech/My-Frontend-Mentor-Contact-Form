c2ecdfaefeab5a285655788c26164b037f75c5f3
Richard Mgbahurike <richardcreatech@gmail.com>
2026-03-12 16:52:30 +0100

    🚀 Making things better one step at a time.

---
+++ my-react-app/src/component/Success.jsx
@@ -0,0 +1,15 @@
+const Successful = () => {
+  return (
+    <article id="success_message">
+      <div className="my_message">
+        <h6> Message Successful</h6>
+        <p>Thanks for completeing the form. We'll be in touch soon</p>
+      </div>
+      <div class="progress-container">
+        <div class="progress-bar"></div>
+      </div>
+    </article>
+  );
+};
+
+export default Successful
